fafc8c4c8017774523807652d03895ae509e638b
Rob Costello <robcostello1@gmail.com>
2023-08-29 14:12:52 +0100

    island visibility 2

---
+++ components/AfloatVid/components/City/City2.stories.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { OrbitControls, Stats } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import City2 from "./City2";
+
+export default {
+  title: "City/City2",
+  component: City2,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof City2>;
+
+const Template: ComponentStory<typeof City2> = (args) => {
+  return (
+    <Canvas
+      camera={{
+        position: [0, 2, 0],
+      }}
+    >
+      <Stats />
+
+      <OrbitControls />
+
+      <ambientLight intensity={0.1} />
+      <directionalLight position={[10, 3, 5]} />
+
+      <City2 {...args} />
+    </Canvas>
+  );
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  setMoving: () => {},
+  size: 600,
+  duration: 120,
+};
