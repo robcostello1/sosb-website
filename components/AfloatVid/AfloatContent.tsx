2e2da3fd0b4f8d930662a6272610962c50ea9aee
Rob Costello <robcostello1@gmail.com>
2023-11-07 21:07:00 +0000

    mobile-compatible pointer lock

--- components/AfloatVid/AfloatContent.tsx
+++ components/AfloatVid/AfloatContent.tsx
@@ -11,10 +11,9 @@ import {
   Box,
   DeviceOrientationControls,
   FirstPersonControls,
-  PointerLockControls,
   Stats,
 } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 
 import { Triplet } from "../../utils/types";
 import { Ocean } from "../Terrain";
@@ -35,6 +34,7 @@ import SkyStreaks from "./components/Sky/SkyStreaks/SkyStreaks";
 import { useSongContext } from "./components/SongProvider";
 import SongProvider from "./components/SongProvider/SongProvider";
 import { PARTS, START_POSITION_Z } from "./consts";
+import { PointerLockControls } from "./vendor/PointerLockControls";
 
 // TODO deprecate in favour of bars
 const parts = {
@@ -53,155 +53,170 @@ const raft = (
   </BobbingItem>
 );
 
-const AfloatContent = () => {
-  const [moving, setMoving] = useState(false);
-  const [showFloatingStuff, setShowFloatingStuff] = useState(false);
-  const [showIslands, setShowIslands] = useState(false);
-  const [showCity, setShowCity] = useState(true);
-  const [showSkyStreaks, setShowSkyStreaks] = useState(false);
-  const [timeSpeedMultiplyer, setTimeSpeedMultiplyer] = useState(0.5);
-  const [showShippingScene, setShowShippingScene] = useState(false);
-  const [showOrb, setShowOrb] = useState(false);
+// type AfloatContentProps = {
+//   canvasRef: React.RefObject<HTMLCanvasElement>;
+// };
 
-  const { handlePlay, barRef } = useSongContext();
+const AfloatContent = () =>
+  // { canvasRef }: AfloatContentProps
+  {
+    const [moving, setMoving] = useState(false);
+    const [showFloatingStuff, setShowFloatingStuff] = useState(false);
+    const [showIslands, setShowIslands] = useState(false);
+    const [showCity, setShowCity] = useState(true);
+    const [showSkyStreaks, setShowSkyStreaks] = useState(false);
+    const [timeSpeedMultiplyer, setTimeSpeedMultiplyer] = useState(0.5);
+    const [showShippingScene, setShowShippingScene] = useState(false);
+    const [showOrb, setShowOrb] = useState(false);
 
-  const handleSetMoving = useCallback(() => {
-    setMoving(true);
-    handlePlay();
-  }, [handlePlay]);
+    const { handlePlay, barRef } = useSongContext();
 
-  useFrame(() => {
-    if (barRef.current > PARTS.verse - 4) {
-      setShowShippingScene(true);
-    }
-    if (barRef.current > PARTS.break - 0.25) {
-      setShowSkyStreaks(true);
-    }
-    if (barRef.current > PARTS.verse2 - 6) {
-      setShowFloatingStuff(true);
-    }
-    if (barRef.current > PARTS.verse2) {
-      setShowCity(false);
-    }
-    if (barRef.current > PARTS.hook) {
-      setShowFloatingStuff(false);
-      setShowSkyStreaks(false);
-      setShowIslands(true);
-      setShowShippingScene(false);
-    }
-    // if (barRef.current > PARTS.chorus - 0.5 && barRef.current < PARTS.chorus) {
-    //   setTimeSpeedMultiplyer(400);
-    // }
-    if (barRef.current > PARTS.chorus - 3) {
-      setTimeSpeedMultiplyer(0.5);
-      setShowOrb(true);
-    }
-    if (barRef.current > PARTS.outro - 0.25) {
-      setShowSkyStreaks(true);
-    }
-    if (barRef.current > PARTS.outro) {
-      // setShowOrb(false);
-      setShowSkyStreaks(false);
-      setTimeSpeedMultiplyer(2);
-      setShowOrb(false);
-    }
-  });
+    const pointerLockControls = useRef<PointerLockControls>();
+    const { camera } = useThree();
 
-  const controls = useRef(null);
+    useEffect(() => {
+      if (!pointerLockControls.current) {
+        pointerLockControls.current = new PointerLockControls(
+          camera,
+          document.body
+        );
+      }
+    }, [pointerLockControls, camera]);
 
-  useEffect(() => {
-    if (controls.current && moving) {
-      // @ts-expect-error // TODO
-      controls.current?.lock?.();
-    }
-  }, [controls, moving]);
+    const handleSetMoving = useCallback(() => {
+      setMoving(true);
+      handlePlay();
+      pointerLockControls.current?.lock?.();
+    }, [handlePlay]);
 
-  const buildingOrb = useMemo(
-    () => <BuildingOrb position={BUILDING_ORB_POSITION} active={showOrb} />,
-    [showOrb]
-  );
+    useFrame(() => {
+      if (barRef.current > PARTS.verse - 4) {
+        setShowShippingScene(true);
+      }
+      if (barRef.current > PARTS.break - 0.25) {
+        setShowSkyStreaks(true);
+      }
+      if (barRef.current > PARTS.verse2 - 6) {
+        setShowFloatingStuff(true);
+      }
+      if (barRef.current > PARTS.verse2) {
+        setShowCity(false);
+      }
+      if (barRef.current > PARTS.hook) {
+        setShowFloatingStuff(false);
+        setShowSkyStreaks(false);
+        setShowIslands(true);
+        setShowShippingScene(false);
+      }
+      // if (barRef.current > PARTS.chorus - 0.5 && barRef.current < PARTS.chorus) {
+      //   setTimeSpeedMultiplyer(400);
+      // }
+      if (barRef.current > PARTS.chorus - 3) {
+        setTimeSpeedMultiplyer(0.5);
+        setShowOrb(true);
+      }
+      if (barRef.current > PARTS.outro - 0.25) {
+        setShowSkyStreaks(true);
+      }
+      if (barRef.current > PARTS.outro) {
+        // setShowOrb(false);
+        setShowSkyStreaks(false);
+        setTimeSpeedMultiplyer(2);
+        setShowOrb(false);
+      }
+    });
 
-  const city = useMemo(
-    () => (
-      <Movement
-        start={-500 * START_POSITION_Z}
-        end={1100}
-        duration={300}
-        moving={moving}
-      >
-        <ShippingScene visible={showShippingScene} position={[0, 0, -320]} />
-        <City
-          visible={showCity}
+    const buildingOrb = useMemo(
+      () => <BuildingOrb position={BUILDING_ORB_POSITION} active={showOrb} />,
+      [showOrb]
+    );
+
+    const city = useMemo(
+      () => (
+        <Movement
+          start={-500 * START_POSITION_Z}
+          end={1100}
           duration={300}
-          sinkStart={parts.break1 - 20}
-          size={500}
           moving={moving}
-          setMoving={handleSetMoving}
+        >
+          <ShippingScene visible={showShippingScene} position={[0, 0, -320]} />
+          <City
+            visible={showCity}
+            duration={300}
+            sinkStart={parts.break1 - 20}
+            size={500}
+            moving={moving}
+            setMoving={handleSetMoving}
+          />
+        </Movement>
+      ),
+      [handleSetMoving, moving, showCity, showShippingScene]
+    );
+
+    return (
+      <Suspense fallback={<></>}>
+        <Stats />
+
+        <FirstPersonControls
+          enabled={!moving}
+          lookSpeed={0.01}
+          movementSpeed={0}
         />
-      </Movement>
-    ),
-    [handleSetMoving, moving, showCity, showShippingScene]
-  );
 
+        <directionalLight
+          color={0x00aaff}
+          intensity={0.07}
+          position={[-10, -2, 2]}
+        />
+        <ambientLight intensity={0.1} />
+
+        <Sky overrideTime={0} timeSpeedMultiplier={timeSpeedMultiplyer} />
+
+        <Ocean />
+
+        <FloatingScene visible={showFloatingStuff} />
+
+        <SkyStreaks visible={showSkyStreaks} numStreaks={20} />
+
+        <BuildingTextureProvider>
+          {city}
+          {buildingOrb}
+        </BuildingTextureProvider>
+
+        <Islands
+          visible={showIslands}
+          scale={200}
+          position={[-105, -3, 0]}
+          bounce={0.2}
+        />
+        <Islands
+          visible={showIslands}
+          scale={200}
+          position={[105, -3, 0]}
+          bounce={0.2}
+        />
+
+        {raft}
+      </Suspense>
+    );
+  };
+
+const AfloatContentWrapper = () => {
+  // const canvasRef = useRef<HTMLCanvasElement>(null);
   return (
-    <Suspense fallback={<></>}>
-      <Stats />
-
-      {/* <PointerLockControls enabled={moving} makeDefault ref={controls} />
-       */}
-
-      <DeviceOrientationControls enabled={moving} makeDefault ref={controls} />
-
-      <FirstPersonControls
-        enabled={!moving}
-        lookSpeed={0.01}
-        movementSpeed={0}
-      />
-
-      <directionalLight
-        color={0x00aaff}
-        intensity={0.07}
-        position={[-10, -2, 2]}
-      />
-      <ambientLight intensity={0.1} />
-
-      <Sky overrideTime={0} timeSpeedMultiplier={timeSpeedMultiplyer} />
-
-      <Ocean />
-
-      <FloatingScene visible={showFloatingStuff} />
-
-      <SkyStreaks visible={showSkyStreaks} numStreaks={20} />
-
-      <BuildingTextureProvider>
-        {city}
-        {buildingOrb}
-      </BuildingTextureProvider>
-
-      <Islands
-        visible={showIslands}
-        scale={200}
-        position={[-105, -3, 0]}
-        bounce={0.2}
-      />
-      <Islands
-        visible={showIslands}
-        scale={200}
-        position={[105, -3, 0]}
-        bounce={0.2}
-      />
-
-      {raft}
-    </Suspense>
+    <Canvas
+      // ref={canvasRef}
+      className={styles.container}
+      shadows
+      gl={{ precision: "mediump" }}
+    >
+      <SongProvider>
+        <AfloatContent
+        // canvasRef={canvasRef}
+        />
+      </SongProvider>
+    </Canvas>
   );
 };
-
-const AfloatContentWrapper = () => (
-  <Canvas className={styles.container} shadows gl={{ precision: "mediump" }}>
-    <SongProvider>
-      <AfloatContent />
-    </SongProvider>
-  </Canvas>
-);
 
 export default AfloatContentWrapper;
